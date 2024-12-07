edd653d7d4c7a258cad28976ca5ac4855d2d6b04
Srinu1912-star <poliboinasrinu1912@gmail.com>
2024-12-07 13:23:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Mobile from './Mobile'
+
+function App(){
+  return(
+    <>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70" 
+      name="Apple iPhone 15 (Black, 128 GB)"
+      price="59999"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/a/c/k/-original-imagtc5fuzkvczr7.jpeg?q=70" 
+      name="Apple iPhone 15 (Pink, 128 GB)"
+      price="59819"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/x/k/h/-original-imah5ywff9rr76zh.jpeg?q=70" 
+      name="SAMSUNG Galaxy S23 FE (Graphite, 128 GB)  (8 GB RAM)#JustHere"
+      price="38999"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/n/l/u/-original-imah2fjd7wfd9ksh.jpeg?q=70" 
+      name="Motorola G85 5G (Olive Green, 128 GB) )"
+      price="29999"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/z/u/9/-original-imah3ajcxshpd2t2.jpeg?q=70" 
+      name="CMF by Nothing Phone 1 (Blue, 128 GB) )"
+      price="19999"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/p/n/u/-original-imah4dyjnzz4wggv.jpeg?q=70" 
+      name="Infinix Hot 50 5G (Sleek Black, 128 GB) )"
+      price="25999"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/6/t/4/-original-imah3chxfkqxyzm3.jpeg?q=70" 
+      name="realme 12x 5G (Woodland Green, 128 GB) )"
+      price="29999"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/p/r/b/nord-ce-3-lite-5g-ce2099-oneplus-original-imagzj42cctpjjze.jpeg?q=70" 
+      name="OnePlus Nord CE 3 Lite 5G (Chromatic Gray, 128 GB) )"
+      price="24999"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/w/3/4/-original-imahyytukhkky5ew.jpeg?q=70" 
+      name="vivo T3x 5G (Crimson Bliss, 128 GB) )"
+      price="19599"/>
+      <Mobile src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/2/y/c/-original-imah4sssdf9pgz3e.jpeg?q=70" 
+      name="SAMSUNG Galaxy A14 5G (Dark Red, 128 GB) )"
+      price="49999"/>
+      
+    </>
+  );
+}
+
+export default App;
